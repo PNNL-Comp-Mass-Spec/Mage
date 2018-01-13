a6b5499c0808e35bbffb5977c1cc88c2780069d2
alchemistmatt <matthew.monroe@pnnl.gov>
2018-01-12 16:02:36 -0800

    Remove old Readme and add distribution script

---
+++ Installer/Distribute_Files.bat
@@ -0,0 +1,17 @@
+xcopy Output\MageInstaller.exe \\floyd\software\Mage /D /Y
+xcopy ..\README.md \\floyd\software\Mage /D /Y
+xcopy ..\RevisionHistory.txt \\floyd\software\Mage /D /Y
+
+xcopy Output\MageInstaller_CBDMS.exe \\cbdms\dms_programs\_Installers /D /Y
+xcopy ..\README.md \\cbdms\dms_programs\_Installers /D /Y
+xcopy ..\RevisionHistory.txt \\cbdms\dms_programs\_Installers /D /Y
+
+xcopy ..\DeployedFiles\MageConcatenator\*.* \\floyd\software\Mage\Exe_Only\MageConcatenator\ /D /Y
+xcopy ..\DeployedFiles\MageExtractor\*.* \\floyd\software\Mage\Exe_Only\MageExtractor\ /D /Y
+xcopy ..\DeployedFiles\MageFilePackager\*.* \\floyd\software\Mage\Exe_Only\MageFilePackager\ /D /Y
+xcopy ..\DeployedFiles\MageFileProcessor\*.* \\floyd\software\Mage\Exe_Only\MageFileProcessor\ /D /Y
+xcopy ..\DeployedFiles\MageMetaDataProcessor\*.* \\floyd\software\Mage\Exe_Only\MageMetaDataProcessor\ /D /Y
+xcopy ..\DeployedFiles\Ranger\*.* \\floyd\software\Mage\Exe_Only\Ranger\ /D /Y
+
+xcopy ..\README.md \\FLOYD\software\Mage\Exe_Only /D /Y
+xcopy ..\RevisionHistory.txt \\FLOYD\software\Mage\Exe_Only /D /Y
