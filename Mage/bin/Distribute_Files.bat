170a1738a3faf0e0130704f39a7a59e54a6adcd3
alchemistmatt <matthew.monroe@pnnl.gov>
2018-06-21 22:50:02 -0700

    Update batch file

--- Mage/bin/Distribute_Files.bat
+++ Mage/bin/Distribute_Files.bat
@@ -1,23 +1,23 @@
 echo Distributing AnyCPU Mage
 
-xcopy Release\Mage.* "F:\My Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Test_Plugins\TestMagePlugIn\bin\Debug\" /Y /D
-xcopy Release\Mage.* "F:\My Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Common\" /Y /D
-xcopy Release\Mage.* "F:\My Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Program\bin\" /Y /D
-xcopy Release\Mage.* "F:\My Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_AScore_PlugIn\bin\Debug\" /Y /D
-xcopy Release\Mage.* "F:\My Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_Mage_PlugIn\bin\Debug\" /Y /D
-xcopy Release\Mage.* "F:\My Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Test_Plugins\TestAScorePlugIn\bin\Debug\" /Y /D
-xcopy Release\Mage.* "F:\My Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_Ape_PlugIn\bin\Debug\" /Y /D
-xcopy Release\Mage.* "F:\My Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_MultiAlign_Aggregator_PlugIn\bin\Debug\" /Y /D
-xcopy Release\Mage.* "F:\My Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Test_Plugins\TestApePlugIn\bin\Debug\" /Y /D
+xcopy Release\Mage.* "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Test_Plugins\TestMagePlugIn\bin\Debug\" /Y /D
+xcopy Release\Mage.* "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Common\" /Y /D
+xcopy Release\Mage.* "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Program\bin\" /Y /D
+xcopy Release\Mage.* "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_AScore_PlugIn\bin\Debug\" /Y /D
+xcopy Release\Mage.* "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_Mage_PlugIn\bin\Debug\" /Y /D
+xcopy Release\Mage.* "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Test_Plugins\TestAScorePlugIn\bin\Debug\" /Y /D
+xcopy Release\Mage.* "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_Ape_PlugIn\bin\Debug\" /Y /D
+xcopy Release\Mage.* "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_MultiAlign_Aggregator_PlugIn\bin\Debug\" /Y /D
+xcopy Release\Mage.* "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Test_Plugins\TestApePlugIn\bin\Debug\" /Y /D
 
-xcopy Release\PRISM.dll "F:\My Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Test_Plugins\TestMagePlugIn\bin\Debug\" /Y /D
-xcopy Release\PRISM.dll "F:\My Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Common\" /Y /D
-xcopy Release\PRISM.dll "F:\My Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Program\bin\" /Y /D
-xcopy Release\PRISM.dll "F:\My Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_AScore_PlugIn\bin\Debug\" /Y /D
-xcopy Release\PRISM.dll "F:\My Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_Mage_PlugIn\bin\Debug\" /Y /D
-xcopy Release\PRISM.dll "F:\My Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Test_Plugins\TestAScorePlugIn\bin\Debug\" /Y /D
-xcopy Release\PRISM.dll "F:\My Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_Ape_PlugIn\bin\Debug\" /Y /D
-xcopy Release\PRISM.dll "F:\My Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_MultiAlign_Aggregator_PlugIn\bin\Debug\" /Y /D
-xcopy Release\PRISM.dll "F:\My Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Test_Plugins\TestApePlugIn\bin\Debug\" /Y /D
+xcopy Release\PRISM.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Test_Plugins\TestMagePlugIn\bin\Debug\" /Y /D
+xcopy Release\PRISM.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Common\" /Y /D
+xcopy Release\PRISM.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Program\bin\" /Y /D
+xcopy Release\PRISM.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_AScore_PlugIn\bin\Debug\" /Y /D
+xcopy Release\PRISM.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_Mage_PlugIn\bin\Debug\" /Y /D
+xcopy Release\PRISM.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Test_Plugins\TestAScorePlugIn\bin\Debug\" /Y /D
+xcopy Release\PRISM.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_Ape_PlugIn\bin\Debug\" /Y /D
+xcopy Release\PRISM.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_MultiAlign_Aggregator_PlugIn\bin\Debug\" /Y /D
+xcopy Release\PRISM.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Test_Plugins\TestApePlugIn\bin\Debug\" /Y /D
 
 pause
