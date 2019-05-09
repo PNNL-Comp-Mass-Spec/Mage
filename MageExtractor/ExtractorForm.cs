67a5e212cfc4aeab5ab86e0fd4eaa28f19cc2cd3
alchemistmatt <matthew.monroe@pnnl.gov>
2019-05-09 13:40:41 -0700

    Updated PRISM.dll with ConsoleMsgUtils methods that support format strings

--- MageExtractor/ExtractorForm.cs
+++ MageExtractor/ExtractorForm.cs
@@ -55,7 +55,7 @@ namespace MageExtractor
             InitializeComponent();
 
             const bool isBetaVersion = false;
-            SetFormTitle("2019-03-04", isBetaVersion);
+            SetFormTitle("2019-05-09", isBetaVersion);
 
             SetTags();
 
