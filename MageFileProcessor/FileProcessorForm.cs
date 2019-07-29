fe43e5c20d8c487ad9617a875cf088fd9357db97
alchemistmatt <matthew.monroe@pnnl.gov>
2019-07-29 13:11:50 -0700

    Update program dates

--- MageFileProcessor/FileProcessorForm.cs
+++ MageFileProcessor/FileProcessorForm.cs
@@ -61,7 +61,7 @@ namespace MageFileProcessor
             InitializeComponent();
 
             const bool isBetaVersion = false;
-            SetFormTitle("2019-07-26", isBetaVersion);
+            SetFormTitle("2019-07-29", isBetaVersion);
 
             SetTags();
 
