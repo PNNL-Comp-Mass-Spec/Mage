8aab4b02e82006739ffda11c16f9c8701bafdb31
alchemistmatt <matthew.monroe@pnnl.gov>
2020-02-22 09:13:06 -0800

    Update batch file

--- Mage/bin/Distribute_Files.bat
+++ Mage/bin/Distribute_Files.bat
@@ -20,4 +20,14 @@ xcopy Release\PRISM.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_
 xcopy Release\PRISM.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_MultiAlign_Aggregator_PlugIn\bin\Debug\" /Y /D
 xcopy Release\PRISM.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Test_Plugins\TestApePlugIn\bin\Debug\" /Y /D
 
+xcopy Release\PRISMDatabaseUtils.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Test_Plugins\TestMagePlugIn\bin\Debug\" /Y /D
+xcopy Release\PRISMDatabaseUtils.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Common\" /Y /D
+xcopy Release\PRISMDatabaseUtils.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Program\bin\" /Y /D
+xcopy Release\PRISMDatabaseUtils.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_AScore_PlugIn\bin\Debug\" /Y /D
+xcopy Release\PRISMDatabaseUtils.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_Mage_PlugIn\bin\Debug\" /Y /D
+xcopy Release\PRISMDatabaseUtils.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Test_Plugins\TestAScorePlugIn\bin\Debug\" /Y /D
+xcopy Release\PRISMDatabaseUtils.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_Ape_PlugIn\bin\Debug\" /Y /D
+xcopy Release\PRISMDatabaseUtils.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_MultiAlign_Aggregator_PlugIn\bin\Debug\" /Y /D
+xcopy Release\PRISMDatabaseUtils.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Test_Plugins\TestApePlugIn\bin\Debug\" /Y /D
+
 pause
