a98f3db7f1260b82a2a92d7d9bae61e776e00a9b
alchemistmatt <matthew.monroe@pnnl.gov>
2020-02-26 18:33:15 -0800

    Updated Batch file

--- DeployedFiles/Distribute_Files.bat
+++ DeployedFiles/Distribute_Files.bat
@@ -6,12 +6,9 @@ xcopy MageExtractor\MageExtContentFilters.dll     F:\Documents\Projects\DataMini
 xcopy MageExtractor\MageExtExtractionFilters.dll  F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Common\ /D /Y
 xcopy Ranger\RangerLib.dll                        F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Common\ /D /Y
 
-xcopy MageExtractor\Mage.dll                      F:\Documents\Projects\JohnSandoval\APE_DLL\Lib\ /D /Y
-xcopy MageExtractor\MageDisplayLib.dll            F:\Documents\Projects\JohnSandoval\APE_DLL\Lib\ /D /Y
-
-xcopy MageExtractor\Mage.dll                      F:\Documents\projects\JohnSandoval\APE_GUI\lib\ /D /Y
-xcopy MageExtractor\MageDisplayLib.dll            F:\Documents\projects\JohnSandoval\APE_GUI\lib\ /D /Y
-xcopy MageExtractor\MageUIComponents.dll          F:\Documents\projects\JohnSandoval\APE_GUI\lib\ /D /Y
+xcopy MageExtractor\Mage.dll                      F:\Documents\Projects\JohnSandoval\APE\lib\ /D /Y
+xcopy MageExtractor\MageDisplayLib.dll            F:\Documents\Projects\JohnSandoval\APE\lib\ /D /Y
+xcopy MageExtractor\MageUIComponents.dll          F:\Documents\Projects\JohnSandoval\APE\lib\ /D /Y
 
 xcopy MageExtractor\Mage.dll                      F:\Documents\projects\JoshAldrich\InterferenceDetection\InterDetect\DLLLibrary\ /D /Y
 xcopy MageExtractor\Mage.dll                      F:\Documents\projects\JoshAldrich\ProteinParsimony\SetCover\Lib\ /D /Y
