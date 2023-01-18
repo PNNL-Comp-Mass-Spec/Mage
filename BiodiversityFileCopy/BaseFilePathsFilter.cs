a1444468ec241cc4f6eb1a136c7d140d3b3f55c4
alchemistmatt <matthew.monroe@pnnl.gov>
2023-01-18 13:50:58 -0800

    Remove unused using directives

--- BiodiversityFileCopy/BaseFilePathsFilter.cs
+++ BiodiversityFileCopy/BaseFilePathsFilter.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Mage;
+﻿using Mage;
 
 namespace BiodiversityFileCopy
 {
--- MageExtContentFilters/FilterResultsBase.cs
+++ MageExtContentFilters/FilterResultsBase.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Data;
 using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
