55693f441e6dc0e41c80b4c7ae775652130b8148
alchemistmatt <matthew.monroe@pnnl.gov>
2024-03-15 14:24:40 -0700

    Update paths in distribution batch file

--- DeployedFiles/Distribute_Files.bat
+++ DeployedFiles/Distribute_Files.bat
@@ -7,13 +7,13 @@ xcopy MageExtractor\MageExtContentFilters.dll     F:\Documents\Projects\DataMini
 xcopy MageExtractor\MageExtExtractionFilters.dll  F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Common\ /D /Y
 xcopy Ranger\RangerLib.dll                        F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Common\ /D /Y
 
-xcopy MageExtractor\Mage.dll                      F:\Documents\Projects\JohnSandoval\APE\lib\ /D /Y
-xcopy MageExtractor\MageDisplayLib.dll            F:\Documents\Projects\JohnSandoval\APE\lib\ /D /Y
-xcopy MageExtractor\MageUIComponents.dll          F:\Documents\Projects\JohnSandoval\APE\lib\ /D /Y
-xcopy Ranger\RangerLib.dll                        F:\Documents\Projects\JohnSandoval\APE\lib\ /D /Y
+xcopy MageExtractor\Mage.dll                      F:\Documents\Projects\John_Sandoval\APE\lib\ /D /Y
+xcopy MageExtractor\MageDisplayLib.dll            F:\Documents\Projects\John_Sandoval\APE\lib\ /D /Y
+xcopy MageExtractor\MageUIComponents.dll          F:\Documents\Projects\John_Sandoval\APE\lib\ /D /Y
+xcopy Ranger\RangerLib.dll                        F:\Documents\Projects\John_Sandoval\APE\lib\ /D /Y
 
-xcopy MageExtractor\Mage.dll                      F:\Documents\projects\JoshAldrich\InterferenceDetection\InterDetect\DLLLibrary\ /D /Y
-xcopy MageExtractor\Mage.dll                      F:\Documents\projects\JoshAldrich\ProteinParsimony\SetCover\Lib\ /D /Y
+xcopy MageExtractor\Mage.dll                      F:\Documents\projects\Josh_Aldrich\InterferenceDetection\InterDetect\DLLLibrary\ /D /Y
+xcopy MageExtractor\Mage.dll                      F:\Documents\projects\Josh_Aldrich\ProteinParsimony\SetCover\Lib\ /D /Y
 
 @echo off
 if not "%1"=="NoPause" pause
