772e4ed8b4a74204c451897dd3e159d34f368391
alchemistmatt <matthew.monroe@pnnl.gov>
2024-11-10 20:35:08 -0800

    Hide deprecated filter set group filter panel in the GUI

--- MageExtractor/ExtractionSettingsPanel.Designer.cs
+++ MageExtractor/ExtractionSettingsPanel.Designer.cs
@@ -44,25 +44,23 @@
             // panel3
             // 
             this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel3.Controls.Add(this.ExtractFromSelectedBtn);
             this.panel3.Controls.Add(this.ExtractFromAllBtn);
+            this.panel3.Controls.Add(this.ExtractFromSelectedBtn);
+            this.panel3.Controls.Add(this.fraMSGFCutoff);
             this.panel3.Controls.Add(this.groupBox3);
             this.panel3.Controls.Add(this.groupBox2);
-            this.panel3.Controls.Add(this.fraMSGFCutoff);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel3.Location = new System.Drawing.Point(7, 6);
-            this.panel3.Margin = new System.Windows.Forms.Padding(4);
+            this.panel3.Location = new System.Drawing.Point(5, 5);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(1174, 91);
+            this.panel3.Size = new System.Drawing.Size(881, 74);
             this.panel3.TabIndex = 0;
             // 
             // ExtractFromSelectedBtn
             // 
             this.ExtractFromSelectedBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.ExtractFromSelectedBtn.Location = new System.Drawing.Point(916, 9);
-            this.ExtractFromSelectedBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.ExtractFromSelectedBtn.Location = new System.Drawing.Point(687, 7);
             this.ExtractFromSelectedBtn.Name = "ExtractFromSelectedBtn";
-            this.ExtractFromSelectedBtn.Size = new System.Drawing.Size(248, 33);
+            this.ExtractFromSelectedBtn.Size = new System.Drawing.Size(186, 27);
             this.ExtractFromSelectedBtn.TabIndex = 3;
             this.ExtractFromSelectedBtn.Text = "Extract Results From &Selected Jobs";
             this.ExtractFromSelectedBtn.UseVisualStyleBackColor = true;
@@ -71,10 +69,9 @@
             // ExtractFromAllBtn
             // 
             this.ExtractFromAllBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.ExtractFromAllBtn.Location = new System.Drawing.Point(916, 49);
-            this.ExtractFromAllBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.ExtractFromAllBtn.Location = new System.Drawing.Point(687, 40);
             this.ExtractFromAllBtn.Name = "ExtractFromAllBtn";
-            this.ExtractFromAllBtn.Size = new System.Drawing.Size(248, 33);
+            this.ExtractFromAllBtn.Size = new System.Drawing.Size(186, 27);
             this.ExtractFromAllBtn.TabIndex = 4;
             this.ExtractFromAllBtn.Text = "Extract Results From &All Jobs";
             this.ExtractFromAllBtn.UseVisualStyleBackColor = true;
@@ -83,11 +80,9 @@
             // groupBox3
             // 
             this.groupBox3.Controls.Add(this.ResultTypeNameCtl);
-            this.groupBox3.Location = new System.Drawing.Point(8, 14);
-            this.groupBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox3.Location = new System.Drawing.Point(6, 11);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox3.Size = new System.Drawing.Size(245, 62);
+            this.groupBox3.Size = new System.Drawing.Size(184, 50);
             this.groupBox3.TabIndex = 0;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Result Type To Extract";
@@ -96,10 +91,9 @@
             // 
             this.ResultTypeNameCtl.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.ResultTypeNameCtl.FormattingEnabled = true;
-            this.ResultTypeNameCtl.Location = new System.Drawing.Point(16, 27);
-            this.ResultTypeNameCtl.Margin = new System.Windows.Forms.Padding(4);
+            this.ResultTypeNameCtl.Location = new System.Drawing.Point(12, 22);
             this.ResultTypeNameCtl.Name = "ResultTypeNameCtl";
-            this.ResultTypeNameCtl.Size = new System.Drawing.Size(220, 24);
+            this.ResultTypeNameCtl.Size = new System.Drawing.Size(166, 21);
             this.ResultTypeNameCtl.TabIndex = 0;
             this.ResultTypeNameCtl.SelectedIndexChanged += new System.EventHandler(this.ResultTypeNameCtl_SelectedIndexChanged);
             // 
@@ -109,21 +103,19 @@
             this.groupBox2.Controls.Add(this.SelectFilterBtn);
             this.groupBox2.Controls.Add(this.FilterSetIDCtl);
             this.groupBox2.Controls.Add(this.KeepResultsCtl);
-            this.groupBox2.Location = new System.Drawing.Point(261, 14);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Location = new System.Drawing.Point(332, 11);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox2.Size = new System.Drawing.Size(451, 62);
+            this.groupBox2.Size = new System.Drawing.Size(349, 50);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Filter For Result Type";
+            this.groupBox2.Text = "Filter For Result Type (deprecated in 2024)";
+            this.groupBox2.Visible = false;
             // 
             // ClearFilterBtn
             // 
-            this.ClearFilterBtn.Location = new System.Drawing.Point(103, 25);
-            this.ClearFilterBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.ClearFilterBtn.Location = new System.Drawing.Point(77, 20);
             this.ClearFilterBtn.Name = "ClearFilterBtn";
-            this.ClearFilterBtn.Size = new System.Drawing.Size(100, 28);
+            this.ClearFilterBtn.Size = new System.Drawing.Size(75, 23);
             this.ClearFilterBtn.TabIndex = 1;
             this.ClearFilterBtn.Text = "Clear";
             this.ClearFilterBtn.UseVisualStyleBackColor = true;
@@ -131,11 +123,9 @@
             // 
             // SelectFilterBtn
             // 
-            this.SelectFilterBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.SelectFilterBtn.Location = new System.Drawing.Point(204, 25);
-            this.SelectFilterBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.SelectFilterBtn.Location = new System.Drawing.Point(153, 20);
             this.SelectFilterBtn.Name = "SelectFilterBtn";
-            this.SelectFilterBtn.Size = new System.Drawing.Size(93, 28);
+            this.SelectFilterBtn.Size = new System.Drawing.Size(70, 23);
             this.SelectFilterBtn.TabIndex = 2;
             this.SelectFilterBtn.Text = "Select...";
             this.SelectFilterBtn.UseVisualStyleBackColor = true;
@@ -144,20 +134,18 @@
             // FilterSetIDCtl
             // 
             this.FilterSetIDCtl.AutoSize = true;
-            this.FilterSetIDCtl.Location = new System.Drawing.Point(37, 31);
-            this.FilterSetIDCtl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.FilterSetIDCtl.Location = new System.Drawing.Point(28, 25);
             this.FilterSetIDCtl.Name = "FilterSetIDCtl";
-            this.FilterSetIDCtl.Size = new System.Drawing.Size(58, 17);
+            this.FilterSetIDCtl.Size = new System.Drawing.Size(50, 15);
             this.FilterSetIDCtl.TabIndex = 0;
             this.FilterSetIDCtl.Text = "All Pass";
             // 
             // KeepResultsCtl
             // 
             this.KeepResultsCtl.AutoSize = true;
-            this.KeepResultsCtl.Location = new System.Drawing.Point(305, 30);
-            this.KeepResultsCtl.Margin = new System.Windows.Forms.Padding(4);
+            this.KeepResultsCtl.Location = new System.Drawing.Point(229, 24);
             this.KeepResultsCtl.Name = "KeepResultsCtl";
-            this.KeepResultsCtl.Size = new System.Drawing.Size(133, 21);
+            this.KeepResultsCtl.Size = new System.Drawing.Size(118, 19);
             this.KeepResultsCtl.TabIndex = 3;
             this.KeepResultsCtl.Text = "Keep All Results";
             this.KeepResultsCtl.UseVisualStyleBackColor = true;
@@ -165,11 +153,9 @@
             // fraMSGFCutoff
             // 
             this.fraMSGFCutoff.Controls.Add(this.MSGFCutoffCtl);
-            this.fraMSGFCutoff.Location = new System.Drawing.Point(720, 14);
-            this.fraMSGFCutoff.Margin = new System.Windows.Forms.Padding(4);
+            this.fraMSGFCutoff.Location = new System.Drawing.Point(196, 11);
             this.fraMSGFCutoff.Name = "fraMSGFCutoff";
-            this.fraMSGFCutoff.Padding = new System.Windows.Forms.Padding(4);
-            this.fraMSGFCutoff.Size = new System.Drawing.Size(173, 62);
+            this.fraMSGFCutoff.Size = new System.Drawing.Size(130, 50);
             this.fraMSGFCutoff.TabIndex = 2;
             this.fraMSGFCutoff.TabStop = false;
             this.fraMSGFCutoff.Text = "MSGF SpecProb Filter";
@@ -179,21 +165,19 @@
             this.MSGFCutoffCtl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.MSGFCutoffCtl.FormattingEnabled = true;
-            this.MSGFCutoffCtl.Location = new System.Drawing.Point(24, 27);
-            this.MSGFCutoffCtl.Margin = new System.Windows.Forms.Padding(4);
+            this.MSGFCutoffCtl.Location = new System.Drawing.Point(18, 22);
             this.MSGFCutoffCtl.Name = "MSGFCutoffCtl";
-            this.MSGFCutoffCtl.Size = new System.Drawing.Size(140, 24);
+            this.MSGFCutoffCtl.Size = new System.Drawing.Size(106, 21);
             this.MSGFCutoffCtl.TabIndex = 0;
             // 
             // ExtractionSettingsPanel
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.panel3);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "ExtractionSettingsPanel";
-            this.Padding = new System.Windows.Forms.Padding(7, 6, 7, 6);
-            this.Size = new System.Drawing.Size(1188, 103);
+            this.Padding = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Size = new System.Drawing.Size(891, 84);
             this.panel3.ResumeLayout(false);
             this.groupBox3.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
