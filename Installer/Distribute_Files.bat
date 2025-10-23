86c2b045238f29b21d872cb33cf078423ec8bc4b
alchemistmatt <matthew.monroe@pnnl.gov>
2025-10-22 18:37:41 -0700

    Update batch distribution file

--- Installer/Distribute_Files.bat
+++ Installer/Distribute_Files.bat
@@ -1,9 +1,14 @@
 xcopy Output\MageInstaller.exe \\floyd\software\Mage /D /Y
 xcopy Output\MageInstaller.exe \\pnl\projects\OmicsSW\Mage /D /Y
+xcopy Output\MageInstaller.exe \\Proto-3\DMS_Programs_Dist\AnalysisToolManagerDistribution\Mage /D /Y
+
 xcopy ..\README.md \\floyd\software\Mage /D /Y
 xcopy ..\README.md \\pnl\projects\OmicsSW\Mage /D /Y
+xcopy ..\README.md \\Proto-3\DMS_Programs_Dist\AnalysisToolManagerDistribution\Mage /D /Y
+
 xcopy ..\RevisionHistory.txt \\floyd\software\Mage /D /Y
 xcopy ..\RevisionHistory.txt \\pnl\projects\OmicsSW\Mage /D /Y
+xcopy ..\RevisionHistory.txt \\Proto-3\DMS_Programs_Dist\AnalysisToolManagerDistribution\Mage /D /Y
 
 xcopy Output\MageInstaller_CBDMS.exe \\cbdms\dms_programs\_Installers /D /Y
 xcopy ..\README.md \\cbdms\dms_programs\_Installers /D /Y
@@ -11,21 +16,35 @@ xcopy ..\RevisionHistory.txt \\cbdms\dms_programs\_Installers /D /Y
 
 xcopy ..\DeployedFiles\MageConcatenator\*.* \\floyd\software\Mage\Exe_Only\MageConcatenator\ /D /Y
 xcopy ..\DeployedFiles\MageConcatenator\*.* \\pnl\projects\OmicsSW\Mage\Exe_Only\MageConcatenator\ /D /Y
+xcopy ..\DeployedFiles\MageConcatenator\*.* \\Proto-3\DMS_Programs_Dist\AnalysisToolManagerDistribution\Mage\Exe_Only\MageConcatenator\ /D /Y
+
 xcopy ..\DeployedFiles\MageExtractor\*.* \\floyd\software\Mage\Exe_Only\MageExtractor\ /D /Y
 xcopy ..\DeployedFiles\MageExtractor\*.* \\pnl\projects\OmicsSW\Mage\Exe_Only\MageExtractor\ /D /Y
+xcopy ..\DeployedFiles\MageExtractor\*.* \\Proto-3\DMS_Programs_Dist\AnalysisToolManagerDistribution\Mage\Exe_Only\MageExtractor\ /D /Y
+
 xcopy ..\DeployedFiles\MageFilePackager\*.* \\floyd\software\Mage\Exe_Only\MageFilePackager\ /D /Y
 xcopy ..\DeployedFiles\MageFilePackager\*.* \\pnl\projects\OmicsSW\Mage\Exe_Only\MageFilePackager\ /D /Y
+xcopy ..\DeployedFiles\MageFilePackager\*.* \\Proto-3\DMS_Programs_Dist\AnalysisToolManagerDistribution\Mage\Exe_Only\MageFilePackager\ /D /Y
+
 xcopy ..\DeployedFiles\MageFileProcessor\*.* \\floyd\software\Mage\Exe_Only\MageFileProcessor\ /D /Y
 xcopy ..\DeployedFiles\MageFileProcessor\*.* \\pnl\projects\OmicsSW\Mage\Exe_Only\MageFileProcessor\ /D /Y
+xcopy ..\DeployedFiles\MageFileProcessor\*.* \\Proto-3\DMS_Programs_Dist\AnalysisToolManagerDistribution\Mage\Exe_Only\MageFileProcessor\ /D /Y
+
 xcopy ..\DeployedFiles\MageMetaDataProcessor\*.* \\floyd\software\Mage\Exe_Only\MageMetaDataProcessor\ /D /Y
 xcopy ..\DeployedFiles\MageMetaDataProcessor\*.* \\pnl\projects\OmicsSW\Mage\Exe_Only\MageMetaDataProcessor\ /D /Y
+xcopy ..\DeployedFiles\MageMetaDataProcessor\*.* \\Proto-3\DMS_Programs_Dist\AnalysisToolManagerDistribution\Mage\Exe_Only\MageMetaDataProcessor\ /D /Y
+
 xcopy ..\DeployedFiles\Ranger\*.* \\floyd\software\Mage\Exe_Only\Ranger\ /D /Y
 xcopy ..\DeployedFiles\Ranger\*.* \\pnl\projects\OmicsSW\Mage\Exe_Only\Ranger\ /D /Y
+xcopy ..\DeployedFiles\Ranger\*.* \\Proto-3\DMS_Programs_Dist\AnalysisToolManagerDistribution\Mage\Exe_Only\Ranger\ /D /Y
 
-xcopy ..\README.md \\FLOYD\software\Mage\Exe_Only /D /Y
+xcopy ..\README.md \\floyd\software\Mage\Exe_Only /D /Y
 xcopy ..\README.md \\pnl\projects\OmicsSW\Mage\Exe_Only /D /Y
-xcopy ..\RevisionHistory.txt \\FLOYD\software\Mage\Exe_Only /D /Y
+xcopy ..\README.md \\Proto-3\DMS_Programs_Dist\AnalysisToolManagerDistribution\Mage\Exe_Only /D /Y
+
+xcopy ..\RevisionHistory.txt \\floyd\software\Mage\Exe_Only /D /Y
 xcopy ..\RevisionHistory.txt \\pnl\projects\OmicsSW\Mage\Exe_Only /D /Y
+xcopy ..\RevisionHistory.txt \\Proto-3\DMS_Programs_Dist\AnalysisToolManagerDistribution\Mage\Exe_Only /D /Y
 
 @echo off
 echo.
